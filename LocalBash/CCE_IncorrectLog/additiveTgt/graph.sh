a2c62987237a88b03b743b5c58d834c75be711ab
Suave101 <adoyle2025@my.fit.edu>
2026-01-30 13:07:40 -0500

    Include Figures and Logs

---
+++ LocalBash/CCE_IncorrectLog/additiveTgt/graph.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# --- Slurm Job Configuration ---
+#SBATCH --job-name=graphing
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --output=graph.log
+#SBATCH --partition=eternity
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=8GB
+#
+    
+# --- Job Execution ---
+echo "----------------------------------------------------"
+echo "Slurm Job ID: $SLURM_JOB_ID"
+echo "Running on host: $(hostname)"
+echo "Start Time: $(date)"
+echo "----------------------------------------------------"
+
+
+export PYTHONNOUSERSITE=1
+
+echo "Initializing conda for script..."
+source /home1/adoyle2025/miniconda3/etc/profile.d/conda.sh
+
+conda activate ml_project
+
+echo "Conda environment activated and isolated:"
+conda info --env
+
+cd /home1/adoyle2025/Distribution-Shift-Lane-Perception
+
+echo "Starting Unit Test..."
+
+python visualizeResultsv4.py
+
+echo "Done!"
---
+++ LocalBash/CCE_IncorrectLog/additiveTgtMicro/ATC6.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# --- Slurm Job Configuration ---
+#SBATCH --job-name=ATC6
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --output=ATC6.log
+#SBATCH --partition=gpu2
+#SBATCH --gres=gpu:4
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=128G
+#
+    
+# --- Job Execution ---
+echo "----------------------------------------------------"
+echo "Slurm Job ID: $SLURM_JOB_ID"
+echo "Running on host: $(hostname)"
+echo "Start Time: $(date)"
+echo "----------------------------------------------------"
+
+
+export PYTHONNOUSERSITE=1
+
+echo "Initializing conda for script..."
+source /home1/adoyle2025/miniconda3/etc/profile.d/conda.sh
+
+conda activate ml_project
+
+echo "Conda environment activated and isolated:"
+conda info --env
+
+cd /home1/adoyle2025/Distribution-Shift-Lane-Perception
+
+echo "Starting Unit Test..."
+
+python shift_concat_experiment.py \
+    --source_dir /home1/adoyle2025/Datasets/Datasets/CULane \
+    --target_dir /home1/adoyle2025/Datasets/Datasets/Curvelanes \
+    --source_list_path /home1/adoyle2025/Datasets/Datasets/CULane/list/train.txt \
+    --target_list_path /home1/adoyle2025/Datasets/Datasets/Curvelanes/train/train.txt \
+    --source_test_list_path /home1/adoyle2025/Datasets/Datasets/CULane/list/test.txt \
+    --src_samples 1000 \
+    --tgt_samples 100 \
+    --ratio_src_samples 10 \
+    --ratio_tgt_samples 5 \
+    --num_runs 100 \
+    --block_idx 4 \
+    --seed_base 32 \
+    --batch_size 4096 \
+    --file_name "mixed_shift_experiment_results_additive_target_micro.json"
+
+echo "----------------------------------------------------"
+echo "Job finished: $(date)"
+echo "----------------------------------------------------"
