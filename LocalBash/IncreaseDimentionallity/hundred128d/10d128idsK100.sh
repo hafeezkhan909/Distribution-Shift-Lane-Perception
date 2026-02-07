488ed544aa9b74befe9992e72ac77432d83c109d
Suave101 <57422635+Suave101@users.noreply.github.com>
2026-02-07 14:08:28 -0500

    bashGen4

---
+++ LocalBash/IncreaseDimentionallity/hundred128d/10d128idsK100.sh
@@ -0,0 +1,57 @@
+#/bin/bash
+
+# --- Slurm Job Configuration ---
+#SBATCH --job-name=10d128idsK100
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --output=10d128idsK100.log
+#SBATCH --partition=gpu2
+#SBATCH --gres=gpu:4
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=128G
+#
+    
+# --- Job Execution ---
+echo "----------------------------------------------------"
+echo "Slurm Job ID: $SLURM_JOB_ID"
+echo "Running on host: $(hostname)"
+echo "Start Time: $(date)"
+echo "Experiment 10: Src=90% (90 samples), Tgt=10% (10 samples)"
+echo "----------------------------------------------------"
+
+
+export PYTHONNOUSERSITE=1
+
+echo "Initializing conda for script..."
+source /home1/adoyle2025/miniconda3/etc/profile.d/conda.sh
+
+conda activate ml_project
+
+echo "Conda environment activated and isolated:"
+conda info --env
+
+cd /home1/adoyle2025/Distribution-Shift-Lane-Perception
+
+echo "Starting Unit Test..."
+
+python shift_concat_experiment.py \
+    --source_dir /home1/adoyle2025/Datasets/Datasets/CULane \
+    --target_dir /home1/adoyle2025/Datasets/Datasets/Curvelanes \
+    --source_list_path /home1/adoyle2025/Datasets/Datasets/CULane/list/train.txt \
+    --target_list_path /home1/adoyle2025/Datasets/Datasets/Curvelanes/train/train.txt \
+    --source_test_list_path /home1/adoyle2025/Datasets/Datasets/CULane/list/test.txt \
+    --src_samples 100 \
+    --tgt_samples 100 \
+    --ratio_src_samples 90 \
+    --ratio_tgt_samples 10 \
+    --num_runs 100 \
+    --block_idx 4 \
+    --seed_base 32 \
+    --batch_size 2048 \
+    --dConfig "d128ids" \
+    --save_all_image_paths True \
+    --file_name "d128idsK100.json"
+
+echo "----------------------------------------------------"
+echo "Job finished: $(date)"
+echo "----------------------------------------------------"
